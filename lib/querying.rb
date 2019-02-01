9389e86bd4e2970a69e1ea193ecdca96b09d7325
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-02-01 02:36:41 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name FROM characters
-  JOIN character_books ON character_books.characters_id = characters.id
+  JOIN character_books ON character_books.character_id = characters.id
   GROUP BY character_books.name
   ORDER BY COUNT(*) DESC LIMIT 1;
   "
